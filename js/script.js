6a0d096f9b673c866f724e308565f0ce9235b847
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-05 00:19:37 +0100

    dichiaro variabili

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Dichiaro le variabili degli input
+const userKilometer = document.getElementById("chilometri-utente");
+
+const userAge = document.getElementById("eta-utente");
+
+// Salvo il button
+const calcButton = document.getElementById("calc");
+
+// Dichiaro le variabili del prezzo base e quello finale
+
+const prezzoFisso = userKilometer * 0.21;
+
+let prezzoFinale;
+
