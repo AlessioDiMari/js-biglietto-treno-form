b1839b957e54e0123b613af394b45d4620e84e39
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-02-05 02:00:32 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -11,6 +11,8 @@ va applicato uno sconto del 40% per gli over 65.
 
 
 // Dichiaro le variabili degli input
+const userName = document.getElementById("nome-utente");
+
 const userKilometer = document.getElementById("chilometri-utente");
 
 const userAge = document.getElementById("eta-utente");
@@ -18,15 +20,16 @@ const userAge = document.getElementById("eta-utente");
 // Salvo il button
 const calcButton = document.getElementById("calc");
 
-// Salvo il risultato
-const result = document.getElementById("risultato");
 
+
+
+// Salvo nome passeggero, costo e offerta
+let passengerName = document.getElementById("nome-passeggero");
+let option = document.getElementById("offerta");
+let price = document.getElementById("costo")
 
 
 // Dichiaro le variabili del prezzo base e quello finale
-
-
-
 let prezzoFinale;
 
 // creo una funzione che parta al "click" del button
@@ -36,6 +39,10 @@ calcButton.addEventListener("click",
     function(){
 
         const prezzoFisso = userKilometer.value * 0.21;
+
+        // Stampo in pagina nel biglietto il nome dell'utente
+        passengerName.innerHTML = userName.value
+
         
         // Se l'utente ha meno di 18 anni verrà applicato
         // uno sconto del 20%
@@ -43,27 +50,36 @@ calcButton.addEventListener("click",
 
             prezzoFinale = prezzoFisso - (prezzoFisso * 0.2);
 
-            result.innerHTML = "Hai meno di 18 anni, hai diritto ad uno sconto del 20%!<br>"
+            option.innerHTML = "Under 18";
+
+            price.innerHTML = `${prezzoFinale}€`;
 
         } else if (userAge.value > 65){
             // Se l'utente ha più di 65 anni verrà applicato
             // uno sconto del 40%
             prezzoFinale = prezzoFisso - (prezzoFisso * 0.4);
 
-            result.innerHTML = "Hai più di 65 anni, hai diritto ad uno sconto del 40%!<br>"
+            option.innerHTML = "Over 65";
+
+            price.innerHTML = `${prezzoFinale}€`;
 
         } else {
             // Se nessuna delle due condizioni precedenti
             // non verrà soddisfatta non si applicherà nessuno sconto
             prezzoFinale = prezzoFisso;
 
+            option.innerHTML = "Standard";
+
+            price.innerHTML = `${prezzoFinale}€`;
+
         }
 
         // Scrivo il risultato con due cifre decimali
         prezzoFinale = prezzoFinale.toFixed(2)
 
-        result.innerHTML += `Il pagamento viene di ${prezzoFinale}€`
 
+        // Mostro il biglietto
+        document.getElementById("risultato").style.display = "block";
     }
 
 )
